20e37a257b4bfed5bae28e1391a12873d57ad16a
James Haskell <jhaskell@jhaskell.net>
2013-09-09 21:15:22 -0700

    Removing default sf bundle, adding fos/user
    okay smartmailer I guess you can come back too

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,10 +15,12 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new GBS\EntityBundle\GBSEntityBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();

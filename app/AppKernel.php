5dfa0fbe5f90ca1ff598f92c9d311d86a862a313
James Haskell <jhaskell@jhaskell.net>
2013-09-11 16:12:17 -0700

    Adds css/js assets to main template, fixes mongo User doc

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Bmatzner\FoundationBundle\BmatznerFoundationBundle(),
             new Bmatzner\ZeptoBundle\BmatznerZeptoBundle(),
